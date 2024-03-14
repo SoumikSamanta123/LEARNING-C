f30a7a6774b1bc0f26bfdf20a61a68a8a8036943
SoumikSamanta2023_30 <samantasoumik26@gmail.com>
2024-03-15 00:50:09 +0530

    2nd practice

---
+++ Codechef/practice/PCL01_problems_INSTNOODLE.c
@@ -0,0 +1,13 @@
+// Input Format
+// The first and only line of input contains two space-separated integers 
+// X and Y — the number of stoves and the number of minutes, respectively.
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    printf("%d", c);
+    return 0;
+}
