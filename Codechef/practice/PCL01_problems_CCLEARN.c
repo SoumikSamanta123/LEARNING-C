30273b0e158bff5841e2932d0658380ea49cc1c8
SoumikSamanta2023_30 <samantasoumik26@gmail.com>
2024-03-15 00:50:46 +0530

    3rd practice

---
+++ Codechef/practice/PCL01_problems_CCLEARN.c
@@ -0,0 +1,11 @@
+//Input Format
+// The only line of input will contain a single integer 
+// N, denoting the number of languages for which there are courses. 
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int a;
+    scanf("%d", &a);
+    printf("%d",2*a);
+}
