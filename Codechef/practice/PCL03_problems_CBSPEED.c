22ba9822c8f6facc32c7eb0181be61df9def22db
SoumikSamanta2023_30 <samantasoumik26@gmail.com>
2024-03-16 22:36:28 +0530

    4th practice

---
+++ Codechef/practice/PCL03_problems_CBSPEED.c
@@ -0,0 +1,18 @@
+// Chef and Brain Speed
+// In ChefLand, human brain speed is measured in bits per second (bps). Chef has a threshold limit of X bits per second above which his calculations are prone to errors. If Chef is currently working at Y bits per second, is he prone to errors?
+// If Chef is prone to errors print YES, otherwise print NO.
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    // Your code goes here (if needed)
+    if(x<y) {
+        printf("YES");
+       }
+    else {
+    printf("NO");
+    }
+    return 0;
+}
