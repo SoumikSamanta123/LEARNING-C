1993c889a988416f4b849b7a35c5363c9a4c0e74
SoumikSamanta2023_30 <162725188+SoumikSamanta123@users.noreply.github.com>
2024-03-17 20:21:57 +0530

    7th hackerrank

--- Hackrrank/challenges_sum-numbers-c_problem.c
+++ Hackrrank/challenges_sum-numbers-c_problem.c
@@ -14,11 +14,9 @@
 
 int main()
 {
-	int a,b;
+    int a,b;
     float c,d;
     scanf("%d %d %f %f", &a,&b,&c,&d);
-    printf("%d %d\n", a+b,a-b);
-    printf("%.1f %.1f", c+d,c-d);
-    
+    printf("%d %d\n%.1f %.1f", a+b,a-b,c+d,c-d);    
     return 0;
 }
