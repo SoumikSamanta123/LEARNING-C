e4ec318c15a1a129e198d7fe7228edfb5a6ea81d
SoumikSamanta2023_30 <samantasoumik26@gmail.com>
2024-03-17 23:30:40 +0530

    5th practice

---
+++ Codechef/practice/PCL03_problems_MARKSTW.c
@@ -0,0 +1,17 @@
+// Alice and Marks
+// Alice has scored 
+// X marks in her test and Bob has scored 
+// Y marks in the same test. Alice is happy if she scored at least twice the marks of Bob’s score. Determine whether she is happy or not.
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    // Your code goes here (if needed)
+    if (x >=2*y){
+    printf("YES");
+    }
+    else
+    printf("NO");
+    return 0;
+}
