d1a5cc5195bf111e6d810ecd7b6ec0e04523a9f9
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-05 13:42:54 +0530

    my 34th code

---
+++ Codechef/course/LBCL07A_problems_PPSC27B.c
@@ -0,0 +1,11 @@
+//Given an integer, print 0 if its odd, else print 1.
+#include <stdio.h>
+
+int main() {
+    int num = 15;
+    int x = num%2;
+    //Complete the code
+    printf("%d", x == 0);
+
+    return 0;
+}
