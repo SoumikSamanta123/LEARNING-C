a0be55e2dae1404572fce58c8c8d95043630bbc5
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-05 13:11:53 +0530

    my 31st code

---
+++ Codechef/course/LBCL07A_problems_PPSC25.c
@@ -0,0 +1,11 @@
+//Using an assignment operator, output the remainder when length is divided by 3.
+#include <stdio.h>
+
+int main() {
+  int length = 11;
+  // Update your code below this line
+  length %= 3;
+  
+  printf("%d",length);
+  
+}
