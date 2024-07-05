9aae04fb92f63992dda0c1f1f0c6516dde182e78
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-05 13:41:53 +0530

    my 32nd code

---
+++ Codechef/course/LBCL07A_problems_PPSC26.c
@@ -0,0 +1,10 @@
+//You are given two variables height1 and height2 - output 0 if they are equal else 1.
+#include <stdio.h>
+
+int main() {
+  int height1 = 15;
+  int height2 = 13;
+  // Update the '_' below to solve the problem
+  printf("%d", height1 != height2); // returns 1 (true) because 15 is not equal to 13
+  return 0;
+}
