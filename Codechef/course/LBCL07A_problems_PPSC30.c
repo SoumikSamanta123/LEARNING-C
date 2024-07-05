62df5b2ffb9ab036b8f240a72a44c89409019aa4
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-05 13:43:18 +0530

    my 35th code

---
+++ Codechef/course/LBCL07A_problems_PPSC30.c
@@ -0,0 +1,14 @@
+// Solve this problem - 1
+// You are given the age of a person as the variable age.
+// Will this person be able to vote?
+// Output 1 or 0 assuming that the voting age is 
+// 18 i.e., a person's age has to be greater than or equal to 18 to vote.
+
+#include <stdio.h>
+
+int main() {
+  int age = 20;
+  // Update the code below this line
+  printf("%d", age>= 18);
+  
+}
