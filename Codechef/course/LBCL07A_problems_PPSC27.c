58216be4bea63e783c38b8298cffb55a9b9ab63c
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-05 13:42:08 +0530

    my 33th code

---
+++ Codechef/course/LBCL07A_problems_PPSC27.c
@@ -0,0 +1,12 @@
+// You are given a variable height.
+// You are allowed to enter the waterpark only if your height is between 5 and 10.
+// Apply the condition and output 1 (true) or 0 (false) as applicable.
+
+#include <stdio.h>
+
+int main() {
+  int height = 15;
+  // Update the '_' in the code below to solve the problem
+  printf("%d",  height > 5 && height < 10);    
+  return 0;
+}
