58ae3eb585ce7ffe5560b9fa76277614a1587da1
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:42:06 +0530

    my 46th code

---
+++ Codechef/course/LBCL20_problems_LCPPCL115B.c
@@ -0,0 +1,13 @@
+//Printing numbers 10 - 0
+#include <stdio.h>
+
+int main() {
+    // Update the code below this line
+    int i = 10;
+    while (i >= 0) {
+        printf("%d\n",i);
+        i--;
+    }
+    
+    return 0;
+}
