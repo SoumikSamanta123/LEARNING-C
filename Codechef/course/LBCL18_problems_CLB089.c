ad0ef7c4e909cdba4455364e7ed131ca48bb215a
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:08:45 +0530

    my 40th code

---
+++ Codechef/course/LBCL18_problems_CLB089.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    char day[4][10]= {"Monday", "Tuesday", "Wednesday","Thursday"};
+    printf(day[2]);
+    printf("\n");
+    printf(day[3]);
+}
