56c1e84cd3c20e1833dbcd46228e253f6e960f0d
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:07:15 +0530

    my 37th code

---
+++ Codechef/course/LBCL18_problems_CLB086A.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // declare the array and print 3rd month
+    char months[4][10]= {"January", "February", "March", "April"};
+    printf(months[2]);
+}
