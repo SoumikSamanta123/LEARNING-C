59061eb0c1d725dc6dc14a719d8a232faf58c6a1
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:41:26 +0530

    my 44th code

---
+++ Codechef/course/LBCL20_problems_CLB097.c
@@ -0,0 +1,14 @@
+// Complete the code by replacing underscores (__)
+#include <stdio.h>
+
+int main() {
+
+    int i = 0;
+    do {
+        printf("%d\n", i);
+        i++;
+    }
+    while(i < 6);
+
+    
+}
