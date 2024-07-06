72a301b181a425d6ad484f47e86b86a194d7d2f8
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:43:53 +0530

    my 48th code

---
+++ Codechef/course/LBCL20_problems_LCPPCL115D.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Update the code below this line
+    int n = 128;
+    int i = 2;
+    while (i <= 128) {
+        printf("%d\n",i);
+        i *= 2;
+    }
+    return 0;
+}
