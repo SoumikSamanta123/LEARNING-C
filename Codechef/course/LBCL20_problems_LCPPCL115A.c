b30edc0962c08512d3b2db692303e3afb3caa5f3
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:41:47 +0530

    my 45th code

---
+++ Codechef/course/LBCL20_problems_LCPPCL115A.c
@@ -0,0 +1,13 @@
+//Print Numbers 5 - 20
+#include <stdio.h>
+
+int main() {
+    int i = 5; // Initialization
+    
+    while (i<=20){
+        printf("%d\n",i);
+        i++;// Update statement
+    }
+    
+    return 0;
+}
