c950fb4aeb67a5b1884fe8b26becb474bdf3cdb3
SoumikSamanta123 <samantasoumik26@gmail.com>
2024-07-06 13:40:43 +0530

    my 43rd code

---
+++ Codechef/course/LBCL20_problems_CLB096.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    scanf("%d", &num);
+    
+    int a = 0;
+    while (a < num) {
+       // Complete the code
+       printf("%d\n",a);
+       a++;
+    }
+}
